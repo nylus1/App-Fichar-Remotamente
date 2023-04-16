942003062839248eb0156eda3f00eead48b6ef08
nylus1 <ivb.ignacio@gmail.com>
2023-04-16 23:48:16 +0200

    App Fichar Remotamente

---
+++ globalsd.sql
@@ -0,0 +1,78 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 14-04-2023 a las 14:01:24
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `globalsd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user_location`
+--
+
+CREATE TABLE `user_location` (
+  `username` varchar(50) NOT NULL,
+  `location` varchar(999) NOT NULL,
+  `time` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `user` varchar(50) NOT NULL,
+  `pass` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `user`, `pass`) VALUES
+(1, 'admin', 'admin');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ login.php
@@ -0,0 +1,40 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "globalsd";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+// Check if user exists
+$user = $_POST['username'];
+$pass = $_POST['password'];
+
+$stmt = mysqli_prepare($conn, "SELECT * FROM usuarios WHERE user=? AND pass=?");
+mysqli_stmt_bind_param($stmt, "ss", $user, $pass);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
+
+if (mysqli_num_rows($result) == 0) {
+  header("Location: index_fail.html");
+} else {
+  // User exists, insert their location, time and username into the user_location table
+  $location = $_POST['location'];
+  $time = $_POST['time'];
+
+  $stmt = mysqli_prepare($conn, "INSERT INTO user_location (username, location, time) VALUES (?, ?, ?)");
+  mysqli_stmt_bind_param($stmt, "sss", $user, $location, $time);
+  mysqli_stmt_execute($stmt);
+
+  // Redirect to loggedin.html
+  header("Location: loggedin.html");
+}
+
+mysqli_close($conn);
+?>
